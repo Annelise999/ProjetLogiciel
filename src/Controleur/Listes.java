09e7301fcfa27cb36e0f7f2294496dc55270cd27
Mario Yammine <marioyammine@MacBook-Pro-de-Mario.local>
2019-06-06 19:47:20 +0200

    MAJ requetes

--- src/Controleur/Listes.java
+++ src/Controleur/Listes.java
@@ -41,12 +41,18 @@ public class Listes {
         tmp = new ArrayList();
         b1 = new BDD();
     }
+
     
-    public ArrayList<Personne> getPersonne ()
+     public ArrayList<Personne> getPersonne ()
     {
     return a_personne;
     }
-
+     
+     public ArrayList<Classe> getClasse ()
+    {
+    return a_classe;
+    }
+    
     public void MAJ() {
 
         int i;
@@ -61,17 +67,17 @@ public class Listes {
             a_anneescolaire.add(new AnneeScolaire(id));
         }
 
-        tmp = b1.RecupTables("SELECT * FROM ProjetEcole.", "DetailBulletin");
-        if (a_detailbulletin.isEmpty() == false) {
-            a_detailbulletin.clear();
+        tmp = b1.RecupTables("SELECT * FROM ProjetEcole.", "Bulletin");
+        if (a_bulletin.isEmpty() == false) {
+            a_bulletin.clear();
         }
         for (i = 0; i < tmp.size(); i++) {
-            String[] parts5 = tmp.get(i).split(",");
-            int id9 = Integer.parseInt(parts5[0]);
-            int id10 = Integer.parseInt(parts5[1]);
-            int id11 = Integer.parseInt(parts5[2]);
-            float id12 = Float.parseFloat(parts5[4]);
-            a_detailbulletin.add(new DetailBulletin(id9, id10, id11, parts5[3], id12));
+            String[] parts2 = tmp.get(i).split(",");
+            int id2 = Integer.parseInt(parts2[0]);
+            int id3 = Integer.parseInt(parts2[1]);
+            int id4 = Integer.parseInt(parts2[2]);
+            float id5 = Float.parseFloat(parts2[4]);
+            a_bulletin.add(new Bulletin(id2, id3, id4, parts2[3], id5));
         }
 
         tmp = b1.RecupTables("SELECT * FROM ProjetEcole.", "Classe");
@@ -203,11 +209,27 @@ public class Listes {
             tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Classe WHERE nom = '"+ classe + "' ");
             String[] idClasse = tmp.get(0).split(",");
             int idCl = Integer.parseInt(idClasse[0]); //on récupère l'idée de la classe
-            int rang = a_personne.size() -1 ; 
-            int idEl = a_personne.get(rang).getId();
-        
+            int range = a_personne.size() -1 ; 
+            int idEl = a_personne.get(range).getId();
             b1.executeUpdate("INSERT INTO ProjetEcole.inscription (idClasse,idPersonne) VALUES ('" + idCl + "' , '" + idEl + "')");
+            
             MAJ();
+            int rangi = a_inscription.size() -1 ; 
+            int idIns = a_inscription.get(rangi).getId();
+            
+            b1.executeUpdate("INSERT INTO ProjetEcole.Bulletin (idTrimestre, idInscription, appreciation, moyenne) VALUES ('1', '"+idIns+"', '', '0.0')");
+            MAJ();
+    
+            int rangb = a_bulletin.size() -1 ; 
+            int idBul = a_bulletin.get(rangb).getId();
+            
+            for(int i=1; i<6; i++){
+                
+                b1.executeUpdate("INSERT INTO ProjetEcole.DetailBulletin (idBulletin, idEnseignement, appreciation, moyenne) VALUES ('"+idBul+"', '"+i+"', '', '0.0')");
+        
+            }
+            MAJ();
+            
             
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -234,28 +256,160 @@ public class Listes {
 
     }
     
-    public void RechercherPersonne (String nom, String prenom, String type){
+     public void RechercherPersonne (String nom, String prenom, String type){
     for (int i = 0; i < a_personne.size(); i++) {
         if (a_personne.get(i).getNom() == nom && a_personne.get(i).getPrenom() == prenom && a_personne.get(i).getType() == type )
         {System.out.println(a_personne.get(i).getId() + " " + a_personne.get(i).getNom() + " " + a_personne.get(i).getPrenom() + " " + a_personne.get(i).getType());}
     }
         }
+    
+    
+    public void ModifierEleve(int id, String nom, String prenom, String classe ){
+        
+        try{
+            b1.executeUpdate("UPDATE ProjetEcole.Personne SET nom = '"+nom+"', prenom = '"+prenom+"' WHERE Personne.id = '"+id+"' ");
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Classe WHERE nom = '"+ classe + "' ");
+            String[] idClasse = tmp.get(0).split(",");
+            int idCl = Integer.parseInt(idClasse[0]);
+            b1.executeUpdate("UPDATE ProjetEcole.inscription SET idClasse = '"+idCl+"'  WHERE idPersonne = '"+id+"' ");
+        MAJ();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+       
+    }
+    public void AjouterCoursProf(int id, String classe, String discipline){
+        try{
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Classe WHERE nom = '"+ classe + "' ");
+            String[] idClasse = tmp.get(0).split(",");
+            int idCl = Integer.parseInt(idClasse[0]);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Discipline WHERE nom = '"+ discipline + "' ");
+            String[] idDisc = tmp.get(0).split(",");
+            int idDi = Integer.parseInt(idDisc[0]);
+            b1.executeUpdate("INSERT INTO ProjetEcole.Enseignement (idClasse, idDiscipline, idPersonne) VALUES ('"+idCl+"', '"+idDi+"', '"+id+"')");     
+        MAJ();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
 
+    }
+   
+     public void AjouterEvaluation(int idEleve, String classe, String discipline, String appreciation, int note){
+         
+         try{
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Classe WHERE nom = '"+ classe + "' ");
+            String[] idClasse = tmp.get(0).split(",");
+            int idCl = Integer.parseInt(idClasse[0]);
+           tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Discipline WHERE nom = '"+ discipline + "' ");
+            String[] idDisc = tmp.get(0).split(",");
+            int idDi = Integer.parseInt(idDisc[0]);
+           tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Enseignement WHERE idClasse = '"+idCl  + "' AND idDiscipline='"+idDi+"' ");
+            String[] idEns = tmp.get(0).split(",");
+            int idE = Integer.parseInt(idEns[0]);
+          
+            
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.inscription WHERE idPersonne = '"+idEleve+ "' ");
+            String[] idIns = tmp.get(0).split(",");
+            int idI = Integer.parseInt(idIns[0]);
+            System.out.println(idI);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Bulletin WHERE idInscription = '"+idI+ "'");
+            String[] idBul = tmp.get(0).split(",");
+            int idB = Integer.parseInt(idBul[0]);
+            
+            
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.DetailBulletin WHERE idEnseignement = '"+idE+ "' AND idBulletin = '"+idB+"'");
+            String[] idDebu = tmp.get(0).split(",");
+            int idDB = Integer.parseInt(idDebu[0]);
+            
+            b1.executeUpdate("INSERT INTO ProjetEcole.Evaluation (idDetailBulletin, note, appreciation) VALUES ('"+idDB+"', '"+note+"', '"+appreciation+"')");
+          MAJ();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+         
+     }   
+    
 
+     public void EditerBulletin(String classe, String personne, String appreciationg, float moyenneg, String appreciation1, String appreciation2 ,String appreciation3 ,String appreciation4, String appreciation5, float moyenne, String discipline){
+         try{
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Classe WHERE nom = '"+ classe + "' ");
+            String[] idClasse = tmp.get(0).split(",");
+            int idCl = Integer.parseInt(idClasse[0]);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Personne WHERE nom = '"+ personne + "' ");
+            String[] idPers = tmp.get(0).split(",");
+            int idP = Integer.parseInt(idPers[0]);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.inscription WHERE idClasse = '"+idCl  + "' AND idPersonne='"+idP+"' ");
+            String[] idIns = tmp.get(0).split(",");
+            int idI = Integer.parseInt(idIns[0]);
+            b1.executeUpdate("UPDATE ProjetEcole.Bulletin SET appreciation = '"+appreciationg+"', moyenne = '"+moyenneg+"' WHERE idInscription = '"+idI+ "' ");
+            
+            /*tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Bulletin WHERE idInscription = '"+idI+ "' ");
+            String[] idBul = tmp.get(0).split(",");
+            int idB = Integer.parseInt(idBul[0]);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Discipline WHERE nom = '"+ discipline + "' ");
+            String[] idDisc = tmp.get(0).split(",");
+            int idDi = Integer.parseInt(idDisc[0]);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Enseignement WHERE idClasse = '"+idCl+ "' AND idDiscipline='"+idDi+"' ");
+            String[] idEns = tmp.get(0).split(",");
+            int idE = Integer.parseInt(idEns[0]);
+            
+           
+            b1.executeUpdate("UPDATE ProjetEcole.DetailBulletin SET idEnseignement = '"+idE+"', appreciation= '"+appreciation1+"', moyenne = '"+moyenne+"' WHERE idBulletin = '"+idB+"' AND idDiscipline = '"+idDi+"'");*/
+           
+             
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+     }
+         
+         
+         public void EditDetailBulletin(String discipline, String classe, String personne, String appreciation,int idEnseignement, String appreciation, float moyenne){
+             try{
+                  
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Classe WHERE nom = '"+ classe + "' ");
+            String[] idClasse = tmp.get(0).split(",");
+            int idCl = Integer.parseInt(idClasse[0]);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Personne WHERE nom = '"+ personne + "' ");
+            String[] idPers = tmp.get(0).split(",");
+            int idP = Integer.parseInt(idPers[0]);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.inscription WHERE idClasse = '"+idCl  + "' AND idPersonne='"+idP+"' ");
+            String[] idIns = tmp.get(0).split(",");
+            int idI = Integer.parseInt(idIns[0]);
+                 
+                 
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Bulletin WHERE idInscription = '"+idI+ "' ");
+            String[] idBul = tmp.get(0).split(",");
+            int idB = Integer.parseInt(idBul[0]);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Discipline WHERE nom = '"+ discipline + "' ");
+            String[] idDisc = tmp.get(0).split(",");
+            int idDi = Integer.parseInt(idDisc[0]);
+            tmp = b1.RecupRequete("SELECT id FROM ProjetEcole.Enseignement WHERE idClasse = '"+idCl+ "' AND idDiscipline='"+idDi+"' ");
+            String[] idEns = tmp.get(0).split(",");
+            int idE = Integer.parseInt(idEns[0]);
+                 
+            b1.executeUpdate("UPDATE ProjetEcole.DetailBulletin SET idEnseignement = '"+idE+"', appreciation= '"+appreciation+"', moyenne = '"+moyenne+"' WHERE idBulletin = '"+idB+"' AND idDiscipline = '"+idDi+"'");
+                 
+                 
+                 MAJ();
+         } catch (SQLException e) {
+            System.out.println(e.getMessage());
+     }
+}  
+     
     public static void main(String[] args) {
         Listes l1 = new Listes();
         l1.MAJ();
         
-        
-        l1.AfficherClasse(2);
-       
-        l1.AjouterEleve("Croche", "Sarah", "CP1");
-  
-        
+       //l1.AjouterEvaluation(45,"CP1", "Mathematiques", "bien", (int) 0.5);
+          l1.EditerBulletin("CP1", "Sasit", "appreciationg", 0, "appreciation1", "appreciation2", "appreciation3", "appreciation4", "appreciation5", 0, "Mathematique");
+        //l1.ModifierEleve(35,"Mario", "Lito", "CE12");
+        //l1.AfficherClasse(5);
+        //l1.AjouterEleve("Sasit", "Sarah", "CP1");
+        //l1.AjouterCoursProf(38, "CP2", "Musique");
         //l1.SupprimerPersonne("Hina", "Manolo");
-        /*for (int i = 0; i < l1.a_personne.size(); i++) {
+        for (int i = 0; i < l1.a_personne.size(); i++) {
             System.out.println(l1.a_personne.get(i).getId() + " " + l1.a_personne.get(i).getNom() + " " + l1.a_personne.get(i).getPrenom() + " " + l1.a_personne.get(i).getType());
-        }*/
+        }
+        
         
         
 
